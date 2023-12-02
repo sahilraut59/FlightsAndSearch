768a79b0a70d1d0dc9e2b6a94f07c9c7a802f409
sahilraut59 <sahilraut59@gmail.com>
2023-12-02 20:13:03 +0530

    minor fixes in update city function

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -34,11 +34,18 @@ class CityRepository {
   }
   async updateCity(cityId, data) {
     try {
-      const city = await City.update(data, {
-        where: {
-          id: cityId,
-        },
-      });
+      //The below approach also works but it will not return update city object, instead it returns array
+      //if we are using Postgres we can use returning:true property to solve this
+      // const city = await City.update(data, {
+      //   where: {
+      //     id: cityId,
+      //   },
+      // });
+
+      //for getting updated data in mysql we can use the below approach
+      const city = await City.findByPk(cityId);
+      city.name = data.name;
+      await city.save();
       return city;
     } catch (error) {
       console.log("Something went wrong in repository layer");
