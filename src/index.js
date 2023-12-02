e5c7f00a47158f52a33b1a5319af25b375471947
sahilraut59 <sahilraut59@gmail.com>
2023-12-02 19:43:43 +0530

    Added router to create city api

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const { PORT } = require("./config/serverConfig");
-const bodyParser = require("body-parser");
+const ApiRoutes = require("./routes/index");
 
 const setupAndStartServer = async () => {
   const app = express();
 
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
-  const PORT = 3000;
+
+  app.use("/api", ApiRoutes);
+
   app.listen(PORT, async () => {
     console.log(`Server started at ${PORT}`);
   });
